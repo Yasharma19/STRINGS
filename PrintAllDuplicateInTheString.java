c410e4c31ac1f6132d5cbc14dfa827d6735d541b
Yash Sharma <yash304sharma@gmail.com>
2023-07-25 00:08:45 +0530

    Update PrintAllDuplicateInTheString.java

--- PrintAllDuplicateInTheString.java
+++ PrintAllDuplicateInTheString.java
@@ -1,13 +1,21 @@
 import java.util.*;
 
 class HelloWorld {
-    public static void main(String[] args) {
-        String s="Yash Sha";
-        Set S=new HashSet();
+    
+    public static void Duplicate(String s){
+        int count[]=new int[256];
         for(int i=0;i<s.length();i++){
-            if(!S.add(s.charAt(i))){ 
-                System.out.println("Duplicate Element is "+s.charAt(i));
+            count[s.charAt(i)]++;
+        }
+        for(int i=0;i<count.length;i++){
+            if(count[i]!=0 && count[i]!=1){
+                System.out.println((char)(i)+"-"+count[i]+" Times");
             }
         }
+    }
+    
+    public static void main(String[] args) {
+        String s="Yashsharma";     
+        Duplicate(s);
     }
 }
